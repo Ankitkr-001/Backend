9cb4a5828a51445f7cb45c5efec38751de7be166
Ankitkr-001 <antitgolu123@gmail.com>
2025-02-07 00:30:35 +0530

    express setu[

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 // require('dotenv').config({ path : ".env"})
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 connectDB()
+    .then(
+        () => {
+            app.on("error", (err) => {
+                console.error("Error in server:", err);
+                throw err;
+            })
+        }
+    )
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        })
+
+    })
+    .catch((err) => {
+        console.log("Error in Database connection", err);
+
+    })
 
 dotenv.config({
     path: ".env"
