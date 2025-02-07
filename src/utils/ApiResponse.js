82df062b03e763d362de6fae5a8d411e0a37ca07
Ankitkr-001 <antitgolu123@gmail.com>
2025-02-07 10:25:30 +0530

    utils added

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,15 @@
+// For Api response handeling How the Eroor should come
+
+class ApiResponse {
+    constructor(
+        statusCode,
+        message,
+        data = null,
+        
+    ){
+        this.statusCode = statusCode;
+        this.message = message;
+        this.data = data;
+        this.success = statusCode < 400
+    }
+}
